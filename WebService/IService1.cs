45c41bd8d105df2020fcb2f0b31e115bd415f95c
unknown <romain.mard@gmail.com>
2018-06-21 02:12:12 +0200

    a

---
+++ WebService/IService1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        IList<stations> GetStation();
+
+        //[OperationContract]
+        //IList<clients> GetClient();
+
+        //[OperationContract]
+        //IList<answers> GetAnswer();
+
+        //[OperationContract]
+        //IList<questions> GetQuestion();
+
+        [OperationContract]
+        IList<SPI_QuestionAnswer_Result> GetQuestionAnswer();
+
+
+
+
+    }
+
+}
---
+++ WebService/Service1.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+        public IList<SPI_QuestionAnswer_Result> GetQuestionAnswer()
+        {
+            IList<SPI_QuestionAnswer_Result> items;
+            using (var database = new GreenMindedEntities())
+            {
+                database.Configuration.ProxyCreationEnabled = false;
+                items = database.SPI_QuestionAnswer().ToList();
+            }
+            return items.ToList();
+        }
+
+        public IList<stations> GetStation()
+        {
+            IList<stations> items;
+            using (var database = new GreenMindedEntities())
+            {
+                database.Configuration.ProxyCreationEnabled = false;
+                items = database.stations.ToList();
+            }
+            return items.ToList();
+        }
+    }
+}
